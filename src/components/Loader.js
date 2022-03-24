3919e29d3c704938cce7631bd38f596c5991b9a8
Tell396 <vova21473@gmail.com>
2022-03-24 21:04:08 +1000

    Story #5. Fixed some bugs.

---
+++ src/components/Loader.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Button, Container, Grid, Box} from "@material-ui/core";
+
+export default function Loader() {
+    return (
+        <Container>
+            <Grid container
+                  style={{height: window.innerHeight - 50}}
+                  alignItems={"center"}
+                  justifyContent={"center"}
+            >
+                <Grid
+                      container
+                      alignItems={"center"}
+                      direction={"column"}
+                >
+                    <div className="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+                </Grid>
+            </Grid>
+        </Container>
+    );
+};
